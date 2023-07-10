1fa82659a87973effd57739166b910affbadc9de
Kyle-001817 <104860159+Kyle-001817@users.noreply.github.com>
2023-07-10 18:55:46 +0200

    Add files via upload

---
+++ application/views/page_login/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="<?php echo base_url('asset/fonts/icomoon/style.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('asset/css/owl.carousel.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('asset/css/bootstrap.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('asset/css/style.css'); ?>">
+    <title>Login</title>
+  </head>
+  <body>
+  <div class="d-lg-flex half">
+    <div class="bg order-1 order-md-2" style="background-image: url('assets/img/login.jpg');"></div>
+    
+  
